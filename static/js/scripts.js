799b1061e12aebe187329919e450c68ffd092e24
Ethan Orozco <ethan.g.orozco@gmail.com>
2023-05-24 15:47:59 -0600

    struggles

--- static/js/scripts.js
+++ static/js/scripts.js
@@ -7,26 +7,9 @@
 // Use this file to add JavaScript to your project
 
 //Dark mode 
-const toggleSwitch = document.querySelector('.theme-switch input[type="checkbox"]');
-const currentTheme = localStorage.getItem('theme');
+const toggleSwitch = document.querySelector('#toggle-switch');
 
-if (currentTheme) {
-  document.documentElement.setAttribute('data-theme', currentTheme);
-
-  if (currentTheme === 'dark') {
-    toggleSwitch.checked = true;
-  }
-}
-
-function switchTheme(e) {
-  if (e.target.checked) {
-    document.documentElement.setAttribute('data-theme', 'dark');
-    localStorage.setItem('theme', 'dark');
-  }
-  else {
-    document.documentElement.setAttribute('data-theme', 'light');
-    localStorage.setItem('theme', 'light');
-  }
-}
-
-toggleSwitch.addEventListener('change', switchTheme, false); 
+toggleSwitch.addEventListener('change', function() {
+  const body = document.querySelector('body');
+  body.classList.toggle('dark', toggleSwitch.checked);
+});
